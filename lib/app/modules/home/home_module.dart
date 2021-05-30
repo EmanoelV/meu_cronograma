148df6d319c6e5ffe66b49eddfeef00d7affa2b5
Emanoel S. Vieira <dev.emanoel@gmail.com>
2021-05-29 23:55:30 -0300

    add tests and module curso

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,10 +1,15 @@
+import 'package:meu_cronograma/app/modules/home/home_page.dart';
+import 'package:meu_cronograma/app/modules/home/home_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class HomeModule extends Module {
   @override
-  final List<Bind> binds = [];
+  final List<Bind> binds = [
+    Bind.lazySingleton((i) => HomeStore()),
+  ];
 
   @override
-  final List<ModularRoute> routes = [];
-
+  final List<ModularRoute> routes = [
+    ChildRoute('/', child: (_, args) => HomePage()),
+  ];
 }
--- test/app/modules/curso/curso_module_test.dart
+++ test/app/modules/curso/curso_module_test.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_modular_test/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:meu_cronograma/app/modules/home/home_module.dart';
+import 'package:meu_cronograma/app/modules/curso/curso_module.dart';
  
 void main() {
 
   setUpAll(() {
-    initModule(HomeModule());
+    initModule(CursoModule());
   });
 }
